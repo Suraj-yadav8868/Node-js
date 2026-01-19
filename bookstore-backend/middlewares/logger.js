55dabe57af935649f3f45be62d21d77224636483
Surya <mrsurajyadavdj@gmail.com>
2026-01-19 16:06:25 +0530

    Delete bookstore-backend directory

--- bookstore-backend/middlewares/logger.js
+++
@@ -1,6 +0,0 @@
-const logger = (req,res,next)=>{
-    console.log(`request comes from [${req.method}] method by this ${req.url}..`);
-    next();
-};
-
-export default logger;
--- bookstore-backend/routes/bookRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {addBook,readBooks} from '../controllers/bookControllers.js'
-
-const router = express.Router();
-
-router.post("/", addBook);
-router.get("/", readBooks);
-
-export default router;
